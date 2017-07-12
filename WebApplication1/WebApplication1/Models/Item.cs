9e6f2fc9a8456feb8c3dadc466db77200e154bf5
Joejiong <1004691415@qq.com>
2017-07-12 20:38:54 +0800

    haha, guys new repo

---
+++ WebApplication1/WebApplication1/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Item
+    {
+        [Key]
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+       
+
+    }
+}
