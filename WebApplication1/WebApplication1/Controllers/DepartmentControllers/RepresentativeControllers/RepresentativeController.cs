246b91f69de6fb89190c985ff5c7dc8b5780f53e
Trisha C N <trisha.natanael@gmail.com>
2017-07-18 15:29:44 +0800

    added contollers

---
+++ WebApplication1/WebApplication1/Controllers/DepartmentControllers/RepresentativeControllers/RepresentativeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers.DepartmentControllers.EmployeeControllers
+{
+    public class RepresentativeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAllRequistion()
+        {
+            return View();
+        }
+    }
+}
